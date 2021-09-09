878edf395a1c909261c3d4f5d160b363e9827b29
Isay Zetter <59151193+izetter@users.noreply.github.com>
2021-09-09 13:30:13 -0500

    Movies with no poster show no image instead of not-found icon

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // Writing HTML here to make this code more easily reusable
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-	<label><b>Search for a movie</b><input type="text" class="input"></label>
+	<label><b>Search for a movie</b></label>
+	<input type="text" class="input">
 	<div class="dropdown">
 		<div class="dropdown-menu">
 			<div class="dropdown-content results"></div>
@@ -10,9 +11,9 @@ root.innerHTML = `
 	</div>
 `;
 
+const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
-const input = document.querySelector('input');
 
 input.addEventListener('input', debounce(onInput, 500));
 
@@ -25,8 +26,8 @@ async function searchMovies(searchString) {
 			},
 		});
 
-		// The OMBD API response may contain a string property called "Error", which, if present, indicates that it couldn't find movies matching
-		// the searchString. It's not really an error though, the request is still succesful, it's just how they designed the API.
+		// If the OMBD API can't find any matching movies, its response will contain a string property called "Error",
+		// It's not really an error though, the request is still succesful, it's just how they designed the API.
 		if (response.data.Error) return [];
 
 		return response.data.Search;
@@ -38,12 +39,15 @@ async function searchMovies(searchString) {
 async function onInput(evt) {
 	const movies = await searchMovies(evt.target.value);
 
+	resultsWrapper.innerHTML = '';
 	dropdown.classList.add('is-active');
 	for (let movie of movies) {
-		const option = document.createElement('a');
+		const option = document.createElement('a');	// dropdown option
+		const posterSRC = movie.Poster === 'N/A' ? '' : movie.Poster;	// The API assings the string "N/A" when no poster URL is found
+
 		option.classList.add('dropdown-item');
 		option.innerHTML = `
-			<img src="${movie.Poster}">
+			<img src="${posterSRC}">
 			${movie.Title}
 		`;
 
